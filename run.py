b6310bb00e94c673f5da710373ee57142b9ec60a
Piva84 <pierre_wangborg@hotmail.com>
2024-05-15 12:44:30 +0000

    completed the try_to_place_ship_on_grid function

--- run.py
+++ run.py
@@ -45,6 +45,8 @@ ship_positions = [[]]
 # Global variable for alphabet
 alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
+
+
 def validate_grid_and_place_ship(start_row, end_row, start_col, end_col):
     """
     Will check the row or column to see if it is safe to place a ship there.
@@ -65,16 +67,39 @@ def validate_grid_and_place_ship(start_row, end_row, start_col, end_col):
                 grid[r][c] = "O"
     return all_valid
 
-def try_to_place_ship_on_grid():
+
+
+def try_to_place_ship_on_grid(row, col, direction, length):
     """
     Based on direction will call helper method to try and place a ship on the grid.
     Returns validate_grid_and_place_ship which will be True or False.
     """
     global grid_size
 
-    pass
+    start_row, end_row, start_col, end_col = row, row + 1, col, col + 1
+    if direction == "left":
+         if col - length < 0:
+            return False
+        start_col = col - length + 1
+
+    elif direction == "right":
+        if col + length >= grid_size:
+            return False
+        end_col = col + length
+
+    if direction == "up":
+         if row - length < 0:
+            return False
+        start_row = row - length + 1
+
+    elif direction == "down":
+        if row + length >= grid_size:
+            return False
+        end_row = row + length
 
     return validate_grid_and_place_ship(0, 0, 0, 0)
+
+
 
 def create_grid():
     """
