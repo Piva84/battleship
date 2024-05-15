1380a40d663cc158cfd6fa8062d40df1ec1ac437
Piva84 <pierre_wangborg@hotmail.com>
2024-05-15 12:05:49 +0000

    Added the global variables

--- run.py
+++ run.py
@@ -1,3 +1,46 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+import time
+
+"""
+-------BATTLESHIPS-------
+    Pre-reqs: Loops, Strings, Arrays, 2D Arrays, Global Variables, Methods
+    How it will work:
+    1. A 10x10 grid will have 8 ships of variable length randomly placed about
+    2. You will have 50 bullets to take down the ships that are placed down
+    3. You can choose a row and column such as A3 to indicate where to shoot
+    4. For every shot that hits or misses it will show up in the grid
+    5. A ship cannot be placed diagonally, so if a shot hits the rest of
+        the ship is in one of 4 directions, left, right, up, and down
+    6. If all ships are unearthed before using up all bullets, you win
+        else, you lose
+
+    Legend:
+    1. "." = water or empty space
+    2. "O" = part of ship
+    3. "X" = part of ship that was hit with bullet
+    4. "#" = water that was shot with bullet, a miss because it hit no ship
+"""
+
+# Global variable for grid
+grid = [[]]
+
+# Global variable for grid size
+grid_size = 10
+
+# Global variable for number of ships to place
+num_of_ships = 8
+
+# Global variable for bullets left
+bullets_left = 50
+
+# Global variable for game over
+game_over = False
+
+# Global variable for ships sunk
+num_of_ships_sunk = 0
+
+# Global variable for ship positions
+ship_positions = [[]]
+
+# Global variable for alphabet
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
